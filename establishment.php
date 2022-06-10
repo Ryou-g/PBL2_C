9a008c1cd9732f4815715345c31fec2ed931d06f
harusandayo <72899728+harusandayo@users.noreply.github.com>
2022-06-10 10:30:33 +0900

    headerkousin

---
+++ establishment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="establishment.css">
+    <title>設定画面</title>
+</head>
+<body>
+
+    <div class="setting">
+        マイページ<br>
+        サウンド<br>
+        入力ログ<br>
+        着せ替え<br>
+        背景を変える<br>
+    </div>
+</body>
+</html>
