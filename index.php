ea7462677aa443fcd8fd381da53158e9cfb43744
taypayjum <kd1309304@st.kobedenshi.ac.jp>
2022-06-10 13:42:39 +0900

    タイトル修正#2

--- index.php
+++ index.php
@@ -4,7 +4,21 @@
     <meta charset="UTF-8">
     <title>ホーム画面</title>
   </head>
-  <body>
+  <link rel="stylesheet" href="./CSS/index.css">
 
+  <body class="is_fade" >
+    <!-- JS -->
+<script>
+var fixLoad = function(){
+    document.body.classList.remove("is_fade");
+    document.body.classList.remove("is_remove");
+} 
+setTimeout(fixLoad, 1000);
+</script>
+  <img src="./images/background.jpeg" class="background_1">
+  <img src="./images/woman.png" class="background_2">
+
+  <div class="md_transition"></div>
+  
   </body>
 </html>
--- js/title.js
+++ js/title.js
@@ -1,17 +1,6 @@
-.anim-box.fadeup.is-animated {
-    animation: fadeup 1s cubic-bezier(0.33, 1, 0.68, 1) 1 forwards;
-  }
-   
-  @keyframes fadeup {
-    0% {
-      transform: translateY(30px);
-      opacity: 0;
-    }
-    80% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
+const circle = document.querySelector('.circle');
+const addClass = ()=>{
+    circle.classList.toggle('is-animated');
+}
+
+circle.addEventListener('click',addClass ,false);
