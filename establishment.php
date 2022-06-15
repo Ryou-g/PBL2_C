9288f5908b8591b908f7259b8990e88312f52efc
harusandayo <72899728+harusandayo@users.noreply.github.com>
2022-06-15 10:26:35 +0900

    add_header_menu/#6

--- establishment.php
+++ establishment.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="/CSS/establishment.css">
+    <link rel="stylesheet" href="./CSS/establishment.css">
     <title>設定画面</title>
 </head>
 <body>
