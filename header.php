6f3951f6dc07023142316431e8622e4cc5f1ff3d
GOWAMIYA Ryou <kd1309305@st.kobedenshi.ac.jp>
2022-06-15 11:29:36 +0900

    できている画面を結合しました。

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <link rel="stylesheet" href="./CSS/header.css">
+    <header>
+        <!-- ヘッダー -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+      <div class="kuuhaku">
+        <nobr class="rogo">おふろっち</nobr> <!-- ここは後々画像になります -->
+          <strong class="nitiji"><?php echo date('Y') ?>/<?php echo date('m') ?>/<?php echo date('d') ?></strong>
+          </div>
+            <hr class="hr1">
+      
+    </header>
