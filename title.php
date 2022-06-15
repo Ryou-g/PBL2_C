452cca41e4378240ff1b1efd1e366d30eb1c4349
taypayjum <kd1309304@st.kobedenshi.ac.jp>
2022-06-15 09:45:58 +0900

    タイトル画面修正#2

--- title.php
+++ title.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
 <link rel="stylesheet" href="./CSS/title.css">
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fascinate+Inline&family=Fredoka+One&family=Lilita+One&family=Londrina+Outline&family=Rubik+Moonrocks&display=swap" rel="stylesheet">
   <head>
     <meta charset="UTF-8">
     <title>タイトル画面</title>
   </head>
-  <body >
-     
+  <body>
+    
 <div class="md_transition"></div>
   <div>  
+  <div class="rogo" >おふろっち</div> <!-- ここは後々画像になります -->
   <img src="./images/background.jpeg" class="background_1">
-  <img src="./images/woman.png" class="background_2">
-
+  <!--<img src="./images/woman.png" class="background_2">--->
   
-  <div class="fadeup">
-      
+  <div  class="fadeup">
+  
       <a div class="only-background" href="index.php">START</div></a>
         
   </div>
