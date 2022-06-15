f513392941cd0e041eb23d8d1f42e59c960ef1ca
GOWAMIYA Ryou <kd1309305@st.kobedenshi.ac.jp>
2022-06-15 12:02:58 +0900

    フッターの不具合修正

--- index.php
+++ index.php
@@ -6,27 +6,27 @@
   </head>
   <link rel="stylesheet" href="./CSS/index.css">
   <body class="is_fade" >
-  
+
     <!-- JS -->
 <script>
 var fixLoad = function(){
     document.body.classList.remove("is_fade");
     document.body.classList.remove("is_remove");
-} 
+}
 setTimeout(fixLoad, 100);
 </script>
 
   <img src="./images/background.jpeg" class="background_1">
   <img src="./images/woman.png" class="background_2">
   <div class="header">
-  <?php 
+  <?php
 require_once 'header.php';
 ?>
 </div>
   <div class="md_transition"></div>
-  
+
   </body>
-  <?php 
+  <?php
 require_once 'footer.php';
 ?>
 </html>
