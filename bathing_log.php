107562ef1d3605f086b076a9cf822f09d5ccca1b
Akira-Kida <106209201+Akira-Kida@users.noreply.github.com>
2022-06-21 11:45:27 +0900

    一応完成/#13

--- bathing_log.php
+++ bathing_log.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
+ <!DOCTYPE html>
+ <html lang="ja">
+ <head>
     <meta charset="UTF-8">
-    <link href="CSS/bathing_log.css" rel="stylsheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/bathing_log.css">
     <title>log</title>
-</head>
-<body>
-    <div class="log">
+ </head>
+ <body>
+ <div class="log">
         <a>2022/06/01</a>
         <h2>お風呂に入りました</h2>
         <h6>2022/06/01 19:05</h6>
@@ -17,44 +19,5 @@
         <h6>2022/06/02 00:00</h6>
         <a>2022/06/03</a>
     </div>
-
-<!--
-    <div class="time01"><span id="time1"></span></a></div>
-
-    <div class="time02"><span id="time2"></span></div>
-
-
-    <script type="text/javascript">
-    document.getElementById("time1").innerHTML = showTime();
-  
-    function showTime() {
-        var now = new Date();
-        var nowYear = now.getFullYear();
-        var nowMonth = now.getMonth()+1;
-        var nowDate = now.getDate();
-  
-        var text = nowYear + "/" + nowMonth + "/" + nowDate; 
-    
-        return text;
-    }
-    </script>
-
-    <script type="text/javascript">
-    document.getElementById("time2").innerHTML = showTime();
-  
-    function showTime() {
-        var now = new Date();
-        var nowYear = now.getFullYear();
-        var nowMonth = now.getMonth()+1;
-        var nowDate = now.getDate();
-        var nowHours = now.getHours();
-        var nowMinutes = now.getMinutes();
-  
-        var text = nowYear + "/" + nowMonth + "/" + nowDate + " " + nowHours + ":" + nowMinutes; 
-    
-        return text;
-    }
-    </script>
-    !-->
-</body>
-</html>
+ </body>
+ </html>
