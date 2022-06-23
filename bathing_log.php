ef881105714c391c2163652c22ef1497422e98c7
Ryou-g <kd1309305@st.kobedenshi.ac.jp>
2022-06-23 11:05:25 +0900

    入浴ログ画面が完成しました

--- bathing_log.php
+++ bathing_log.php
@@ -15,18 +15,23 @@ require_once 'header.php';
 ?>
 </div>
 <body>
-    <div class="log">
-    <h4>2022/06/01</h4>
-      <h2>お風呂に入りました</h2>
-      <h6>2022/06/01 19:05</h6>
-      <h2>お風呂から出ました</h2>
-      <h6>2022/06/01 19:37</h6>
-      <h4>2022/06/02</h4>
-      <h2>お風呂に入りませんでした</h2>
-      <h6>2022/06/02 00:00</h6>
-      <h4>2022/06/03</h4>
-      <br>
-    </div>
+<table class="setting">
+            <tr>
+              <td class="mojibox"><class="moji">2022/06/01<br>お風呂に入りました　19:05</td>
+            </tr>
+            <tr>
+              <td class="mojibox"><class="moji">お風呂から出ました　19:37</td>
+            </tr>
+            <tr>
+              <td class="mojibox"><class="moji">2022/06/02<br>お風呂に入りませんでした　23:59</td>
+            </tr>
+            <tr>
+              <td class="mojibox"><class="moji">2022/06/03</td>
+            </tr>
+            <tr>
+              <td class="mojibox"><class="moji"></td>
+            </tr>
+        </table>
 </body>
 <div class="footer">
 <?php
