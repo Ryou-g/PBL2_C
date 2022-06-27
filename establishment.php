2ac4105f7e8d9125db4f59f216d66b660327f172
Ryou-g <kd1309305@st.kobedenshi.ac.jp>
2022-06-27 13:11:19 +0900

    フッターの調整と全画面にアニメーション追加

--- establishment.php
+++ establishment.php
@@ -13,24 +13,25 @@
   require_once 'header.php';
   ?>
 </div>
-  <b class="is_fade" >
-  
+  <body class="is_fade" >
+
     <!-- JS -->
 <script>
 var fixLoad = function(){
     document.body.classList.remove("is_fade");
     document.body.classList.remove("is_remove");
-} 
+}
 setTimeout(fixLoad, 100);
 </script>
 
   <img src="./images/background.jpeg" class="background_1">
   <img src="./images/woman.png" class="background_2">
   <div class="header">
-  <?php 
+  <?php
 require_once 'header.php';
 ?>
 </div>
+<div class="md_transition"></div>
         <table class="setting">
             <tr>
                 <td class="mojibox"><a href="my_page.php" class="moji">マイページ</a></td>
@@ -49,7 +50,7 @@ require_once 'header.php';
             </tr>
         </table>
     <div class="footer">
-        <?php 
+        <?php
             require_once 'footer.php';
         ?>
     </div>
