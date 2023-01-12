09ffd9fff36e877bfa1f16ddbff2551387c4777c
Suzumy <ryouga.0085@gmail.com>
2023-01-12 11:14:48 +0900

    bugfix_dressup
    衣装着せ替え画面に関するバグを修正

--- dress_up_db.php
+++ dress_up_db.php
@@ -3,12 +3,15 @@ require_once __DIR__ . '/DBConnect.php';
 
 $user_id = 1; //ログイン機能実装後、セッションから代入する
 $costume_path = $_POST['example'];
+$path_array = explode(".png", $costume_path);
+$path = substr($path_array[0], 0 , -1);
+$path = $path . "1.png";
 
 
 // costume_pathからidを特定
 $sql = "SELECT * FROM Costume WHERE costume_path = :costume_path";
 $stmt = $pdo -> prepare($sql);
-$stmt -> bindvalue(':costume_path', $costume_path);
+$stmt -> bindvalue(':costume_path', $path);
 $stmt->execute();
 $costume = $stmt -> fetch();
 var_dump($costume);
